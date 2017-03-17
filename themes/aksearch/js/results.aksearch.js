40bd52467397865aeb35b32c6be9fbcfe7a1741a
Michael Birkner <michael.birkner@akwien.at>
2017-03-17 19:26:00 +0100

    Began adding facet tip popup.

---
+++ themes/aksearch/js/results.aksearch.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	
+	//akUseFacetsTooltip
+	var useFacetsTooltip = '<div class="akUseFacetsTooltip">Suche einschränken mit Filtern</div>';
+	//useFacetsTooltip += '<div class="arrowDown"></div>';
+	$('.col-sm-3').prepend(useFacetsTooltip)
+})
