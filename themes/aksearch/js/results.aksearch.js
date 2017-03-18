7f0be1f317807bbffce71c53d2bffa5c614c64b7
Michael Birkner <michael.birkner@akwien.at>
2017-03-18 09:44:04 +0100

    Added tip tooltip for facets.

--- themes/aksearch/js/results.aksearch.js
+++ themes/aksearch/js/results.aksearch.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
 	
-	//akUseFacetsTooltip
-	var useFacetsTooltip = '<div class="akUseFacetsTooltip">Suche einschränken mit Filtern</div>';
-	//useFacetsTooltip += '<div class="arrowDown"></div>';
-	$('.col-sm-3').prepend(useFacetsTooltip)
-})
+	// Show tooltip for facets only if search results in more than 100 hits
+	if (VuFind.recordTotal > 100) {
+		
+		// Check if the user already saw this tooltip in this browser session. If not, show it.
+		if (!sessionStorage.akFacetTooltipSeen) {			
+			var akFacetTipTooltip =	'<div class="akTipTooltip" id="akFacetTipTooltip">' +
+										'<div class="akTipTooltipHeader">' +
+											'<div class="akTipTooltipHeaderText">' + vufindString['akTip'] + '</div>' +
+											'<div class="akTipTooltipHeaderClose"><i class="fa fa-times" aria-hidden="true"></i></div>' +
+											'<div class="akClearer"></div>' +
+										'</div>' +
+										'<div class="akTipTooltipText">' +
+											vufindString['facetTiptooltipText'] +
+										'</div>' +
+									'</div>';
+			
+			// Prepend the tooltip to the facet column:
+			$('.col-sm-3').prepend(akFacetTipTooltip);
+			
+			// Slowly show tooltip
+			$('#akFacetTipTooltip').fadeIn(700);
+			
+			$(document).on('click', '#akFacetTipTooltip', function() {
+				// Set sessionStorage.akFacetTooltipSeen to true so that the tooltip does not load again in this browser session (until user closes browser tab)
+				sessionStorage.akFacetTooltipSeen = true;
+				
+				// Remove the tooltip
+				$(this).fadeOut(300, function(){
+					$(this).remove();
+				});
+			});
+			
+		}
+	}
+});
